1df11ea46952ec67e3c93dcd1beec42d5073a8c0
TatyanaShu <tashu529@gmail.com>
2021-03-13 15:45:31 +0300

    feat: add some solution for str

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let x=str.macth(/[\(\[\)\}\{\]]/g)
+    if (x.length%2===0){return true}
+    else {return false)}
+//   for (i=0; i<str.length; i++){
+//     if (str.match(/.\(\[\{/))
+//   }
+
 }
